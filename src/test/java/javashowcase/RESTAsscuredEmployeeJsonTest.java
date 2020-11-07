3be7ba8243a98c24cea60ca69587e8f93b8fd8f3
Bhagi <puppyreddy1997@gmail.com>
2020-11-07 11:16:18 +0530

    [Bhagyalaxmi]Added:Added code and Testcase to perform CURD operations

--- src/test/java/javashowcase/RESTAsscuredEmployeeJsonTest.java
+++ src/test/java/javashowcase/RESTAsscuredEmployeeJsonTest.java
@@ -1,25 +1,73 @@
 package javashowcase;
 
-
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-
-import static io.restassured.RestAssured.get;
 
 public class RESTAsscuredEmployeeJsonTest {
 
-    final static String EMPLOYEE_LIST = " http://localhost:3000/employees";
+    private int empId;
+
+    @Before
+    public  void setUp(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 3000;
+        empId = 9;
+    }
+
+    public Response getEmployeeList(){
+        Response response = RestAssured.get("/employees");
+        return response;
+    }
 
     @Test
     public void onCallingList_ReturnEmployeeList() {
-        Response response = get(EMPLOYEE_LIST + "/list");
+        Response response = getEmployeeList();
         System.out.println("AT FIRST: " + response.asString());
-        response.then().body("id", Matchers.hasItems(1,2));
+        response.then().body("id", Matchers.hasItems(1,3,4));
         response.then().body("name", Matchers.hasItems("pankaj"));
     }
 
-
+    @Test
+    public void givenEmployee_OnPost_ShouldReturnAddedEmployee() {
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"name\": \"Lisa\",\"salary\":\"2000\"}")
+                .when()
+                .post("/employees/create");
+        String respAsStr = response.asString();
+       JsonObject jsonObject = new Gson().fromJson(respAsStr, JsonObject.class);
+       int id = jsonObject.get("id").getAsInt();
+       response.then().body("id", Matchers.any(Integer.class));
+       response.then().body("name", Matchers.is("Lisa"));
+    }
+    @Test
+    public void givenEmployee_OnUpdate_ShouldReturnUpadteEmployee() {
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"name\": \"Lisa Tamaki\",\"salary\":\"2000\"}")
+                .when()
+                .put("/employees/update/"+empId);
+        String respAsStr = response.asString();
+        response.then().body("id", Matchers.any(Integer.class));
+        response.then().body("name", Matchers.is("Lisa Tamaki"));
+        response.then().body("salary", Matchers.is("2000"));
+    }
+    @Test
+    public void givenEmployeeId_OnDelete_ShouldReturnSuccessStarus() {
+        Response response = RestAssured.delete("/employees/delete/" + empId);
+        String respAsStr = response.asString();
+        int statusCode  = response.getStatusCode();
+        MatcherAssert.assertThat(statusCode, CoreMatchers.is(200));
+        response = getEmployeeList();
+        System.out.println("AT END: " + response.asString());
+        response.then().body("id",Matchers.not(empId));
+    }
 }
