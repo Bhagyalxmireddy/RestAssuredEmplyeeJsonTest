adb80b1f691cbabda7fb6fdc5d67894215678128
Bhagi <puppyreddy1997@gmail.com>
2020-11-07 15:30:31 +0530

    [Bhagyalaxmi]Refactered:Refactered code to work the Testcase we replace with hsItem

--- src/test/java/javashowcase/RESTAsscuredEmployeeJsonTest.java
+++ src/test/java/javashowcase/RESTAsscuredEmployeeJsonTest.java
@@ -11,14 +11,26 @@ import static io.restassured.RestAssured.get;
 
 public class RESTAsscuredEmployeeJsonTest {
 
-    final static String EMPLOYEE_LIST = " http://localhost:3000/employees";
+    private int empId;
+
+    @Before
+    public  void setUp(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 3000;
+        empId = 9;
+    }
+
+    public Response getEmployeeList(){
+        Response response = RestAssured.get("/employees");
+        return response;
+    }
 
     @Test
     public void onCallingList_ReturnEmployeeList() {
-        Response response = get(EMPLOYEE_LIST + "/list");
+        Response response = getEmployeeList();
         System.out.println("AT FIRST: " + response.asString());
         response.then().body("id", Matchers.hasItems(1,2));
-        response.then().body("name", Matchers.hasItems("pankaj"));
+        response.then().body("name", Matchers.hasItem("Pankaj"));
     }
 
 
